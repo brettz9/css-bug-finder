33c600740ab4189b1a77c665eafdd32f18dcd2ba
Brett Zamir <brettz9@yahoo.com>
2020-11-01 20:35:06 +0800

    - Demo use of stylelint + plugin to detect undeclared custom properties (i.e., undeclared CSS variables)

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  plugins: [
+    'stylelint-value-no-unknown-custom-properties'
+  ],
+  rules: {
+    'csstools/value-no-unknown-custom-properties': true
+  }
+};
